730853ce7d860f7f658f7e98cec23f73fc7fab24
paulobtn <paulonetodev@gmail.com>
2021-10-02 17:04:23 -0300

    Fiz uso do hook useRequest em Product.js
    No futuro ainda posso criar componentes para erros e loading,
    mas pretendo padronizar essa forma de fazer requests e usar
    um switch para avaliar o estado do request e escolher o que
    renderizar.

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,17 +1,32 @@
 import { useParams } from 'react-router-dom';
 
 import useRequest from '../hooks/useRequest';
+import {IDLE, PENDING, RESOLVED, REJECTED} from '../hooks/useRequest';
 
 const Product = () => {
 
   let {id} = useParams();
-  let {response} = useRequest({url: `/api/games/${id}`, method: 'get'});
+  let {response} = useRequest({
+    url: `/api/games/${id}`,
+    method: 'get'
+  });
 
   console.log(response);
+
+  const renderProduct = () => {
+    switch(response.status){
+      case RESOLVED:
+        return (
+          <div>Jogo {id}</div>
+        )
+      case REJECTED:
+        return response.error.response.statusText;
+      default:
+        return 'loading...'
+    }
+  }
   
-  return (
-    <div>Jogo {id}</div>
-  );
+  return(renderProduct());
 }
 
 export default Product;

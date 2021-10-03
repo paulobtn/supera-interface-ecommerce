a3301dcbd712308c264aea64469950391c19e460
paulobtn <paulonetodev@gmail.com>
2021-10-03 15:32:33 -0300

    Testei o reducer quando a ação é REMOVE_FROM_CART

--- client/src/__tests__/reducers/reducers.test.js
+++ client/src/__tests__/reducers/reducers.test.js
@@ -2,7 +2,7 @@ import configureMockStore from 'redux-mock-store';
 
 import cartReducer from '../../reducers/cartReducer';
 import {initState} from '../../reducers/cartReducer';
-import { ADD_TO_CART } from "../../actions/types";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../../actions/types";
 
 
 describe('Testa reducer', () => {
@@ -22,12 +22,41 @@ describe('Testa reducer', () => {
         "score": 80,
         "image": "call-of-duty-infinite-warfare.png"
       };
-
+      
       expect (cartReducer(initState, {
+        type: ADD_TO_CART,
+        payload: {
+          quantity: 10,
+          data: game
+        }
+      })).toMatchSnapshot();  
+
+    })
+
+    it('retornando estado correto após remover um jogo', () => {
+
+      let game = {
+        "id": 201,
+        "name": "Call Of Duty Infinite Warfare",
+        "price": 49.99,
+        "score": 80,
+        "image": "call-of-duty-infinite-warfare.png"
+      };
+      
+      // adiciona no carrinho
+      let state = cartReducer(initState, {
         type: ADD_TO_CART,
         payload: {
           quantity: 1,
           data: game
+        }
+      });  
+
+      // testa a remoção
+      expect (cartReducer(initState, {
+        type: REMOVE_FROM_CART,
+        payload: {
+          id: 201
         }
       })).toMatchSnapshot();  
 
